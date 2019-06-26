103f20d5f3b18c143b30f2bfe4713ea286eff16d
Vivek Raman <vr.ac4bf@live.com>
2019-06-27 01:35:11 +0530

    Initial commit

---
+++ Assets/Scripts/MarkerGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerGenerator : MonoBehaviour
+{
+    public GameObject MarkerPrefab = null;
+    [Min(1)] public int Divisions = 12;
+
+    private List<Vector3> positions = null;
+
+    private void Start()
+    {
+        StartCoroutine(Generate());
+    }
+
+    private GameObject Spawn(Quaternion rot)
+    {
+        return Instantiate(MarkerPrefab, this.transform.position, rot, this.transform);
+    }
+
+    private IEnumerator Generate()
+    {
+        float angleDiv = 360 / Divisions;
+        for (int i = 0; i <= Divisions - 1; ++i)
+        {
+            Spawn(Quaternion.Euler(new Vector3(0f, 0f, angleDiv * i)));
+            yield return new WaitForSeconds(0.2f);
+        }
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/ShowTime.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTime : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject HourHand = null;
+    public GameObject MinuteHand = null;
+
+    [Header("Hand Speeds")]
+    [SerializeField] [Range(0, 1)] private float HourSpeed = 0.1f;
+    [SerializeField] [Range(0, 1)] private float MinSpeed = 0.1f;
+
+    [Header("Debug")]
+    [SerializeField] private float HourTarget = 0f;
+    [SerializeField] private float MinTarget = 0f;
+
+    [SerializeField] private bool isTimeAuto = true;
+    [SerializeField] [Range(0, 23)] private int hour = 0;
+    [SerializeField] [Range(0, 59)] private int min = 0;
+
+    private void Start()
+    {
+        SetTargetAngles();
+        HourHand.transform.rotation = Quaternion.Euler(0f, 0f, HourTarget);
+        MinuteHand.transform.rotation = Quaternion.Euler(0f, 0f, MinTarget);
+    }
+
+    private void Update()
+    {
+        SetTargetAngles();
+        HourHand.transform.rotation = Quaternion.RotateTowards(HourHand.transform.rotation, Quaternion.Euler(0f, 0f, HourTarget), HourSpeed);
+        MinuteHand.transform.rotation = Quaternion.RotateTowards(MinuteHand.transform.rotation, Quaternion.Euler(0f, 0f, MinTarget), MinSpeed);
+    }
+
+    private void SetTargetAngles()
+    {
+        if (isTimeAuto)
+        {
+            hour = System.DateTime.Now.Hour;
+            min = System.DateTime.Now.Minute;
+        }
+        HourTarget = (hour % 12) * 30;          // 1 hr = 30 degrees
+        MinTarget = min * 6;                    // 1 min = 6 degrees
+    }
+}
